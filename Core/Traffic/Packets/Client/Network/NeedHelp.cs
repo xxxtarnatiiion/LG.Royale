c1760599c0124d9afe1ccb9b3f1fae67626d54c7
xxxtarnatiiion <97824579+xxxtarnatiiion@users.noreply.github.com>
2022-01-16 16:56:13 +0900

    Create NeedHelp.cs

---
+++ Core/Traffic/Packets/Client/Network/NeedHelp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using LG.Barbarian.Hype;
+
+namespace LG.Barbarian.Traffic
+{
+    class NeedHelp
+    {
+
+        /// <summary>
+        /// Packet's ID.
+        /// </summary>
+        public static int Identifier
+        {
+            get
+            {
+                return 10108;
+            }
+        }
+
+        /// <summary>
+        /// Packet's new instance.
+        /// </summary>
+        public NeedHelp()
+        {
+            this.Process();
+        }
+
+        /// <summary>
+        /// Processes this instance.
+        /// </summary>
+        public void Process()
+        {
+            new KeepAlive();
+        }
+    }
+}
