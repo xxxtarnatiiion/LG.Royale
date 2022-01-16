411980615ea6f2046230e28899475d5042395f71
xxxtarnatiiion <97824579+xxxtarnatiiion@users.noreply.github.com>
2022-01-16 17:14:03 +0900

    Create KeepAliveOk.cs

---
+++ Hype/Traffic/Packets/Server/Account/KeepAliveOk.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using LG.Royale.Hype;
+using LG.Royale.Network;
+
+namespace LG.Royale.Packets
+{
+    class KeepAliveOk
+    {
+
+        /// <summary>
+        /// Packet's ID.
+        /// </summary>
+        public int Identifier
+        {
+            get
+            {
+                return 20108;
+            }
+        }
+
+        /// <summary>
+        /// Packet's version.
+        /// </summary>
+        public int Version
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Packet's writer.
+        /// </summary>
+        public Writer Writer;
+
+        /// <summary>
+        /// Packet's new instance.
+        /// </summary>
+        public KeepAliveOk()
+        {
+            this.Writer = new Writer();
+        }
+
+        /// <summary>
+        /// Encodes this instance.
+        /// </summary>
+        private byte[] Encode()
+        {
+            return this.Writer.Patch(this.Identifier, this.Version);
+        }
+
+        /// <summary>
+        /// Sends this instance.
+        /// </summary>
+        public void Send()
+        {
+            Gateway.Send(this.Encode());
+        }
+    }
+}
