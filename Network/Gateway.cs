2d32ae6e9ab115b5d79caaa26fe1f3a6485d8559
xxxtarnatiiion <97824579+xxxtarnatiiion@users.noreply.github.com>
2022-01-16 17:18:36 +0900

    Create Gateway.cs

---
+++ Network/Gateway.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+using LG.Barbarian.Hype;
+
+namespace LG.Barbarian.Network
+{
+    class Gateway
+    {
+
+        /// <summary>
+        /// Gateway's sockets.
+        /// </summary>
+        public static Socket Server, Client;
+
+        /// <summary>
+        /// Gateway's encryption tools.
+        /// </summary>
+        public static Rivest Encrypter, Decrypter;
+
+        /// <summary>
+        /// Listens to the specified address.
+        /// </summary>
+        /// <param name="IPAddress"></param>
+        /// <param name="Port"></param>
+        public static void Start(IPAddress IPAddress, int Port)
+        {
+            Encrypter = new Rivest();
+            Decrypter = new Rivest();
+
+            Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Server.Bind(new IPEndPoint(IPAddress, Port));
+            Server.Listen(150);
+
+            Program.Log("Socket has been started.", false);
+            Program.Log("Server has been started.", false);
+            Program.Log(null);
+            
+            while (true)
+            {
+                Client = Server.Accept();
+                break;
+            }
+
+            Protocol.Follow();
+        }
+    }
+}
