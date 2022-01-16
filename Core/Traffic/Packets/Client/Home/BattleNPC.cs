2f89b0884620fac7d48b514f9a369a0c04720bd1
xxxtarnatiiion <97824579+xxxtarnatiiion@users.noreply.github.com>
2022-01-16 16:55:07 +0900

    Create BattleNPC.cs

---
+++ Core/Traffic/Packets/Client/Home/BattleNPC.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using LG.Barbarian.Hype;
+
+namespace LG.Barbarian.Traffic
+{
+    class BattleNPC
+    {
+
+        /// <summary>
+        /// Packet's ID.
+        /// </summary>
+        public static int Identifier
+        {
+            get
+            {
+                return 14104;
+            }
+        }
+
+        /// <summary>
+        /// Packet's new instance.
+        /// </summary>
+        public BattleNPC()
+        {
+            this.Process();
+        }
+
+        /// <summary>
+        /// Processes this instance.
+        /// </summary>
+        public void Process()
+        {
+            new AcceptNPC();
+        }
+    }
+}
