ca92e2a3236fec69838c61614b937ae8737b778a
xxxtarnatiiion <97824579+xxxtarnatiiion@users.noreply.github.com>
2022-01-16 16:53:03 +0900

    Create SendFreeChest.cs

---
+++ Core/Traffic/Commands/Server/Home/SendFreeChest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using LG.Barbarian.Hype;
+using LG.Barbarian.Logic;
+
+namespace LG.Barbarian.Traffic
+{
+    class SendFreeChest
+    {
+
+        /// <summary>
+        /// Packet's type.
+        /// </summary>
+        public int Type
+        {
+            get
+            {
+                return 210;
+            }
+        }
+
+        /// <summary>
+        /// Packet's writer.
+        /// </summary>
+        public Writer Writer;
+
+        /// <summary>
+        /// Packet's new instance.
+        /// </summary>
+        public SendFreeChest()
+        {
+            this.Writer = new Writer();
+            this.Process();
+        }
+
+        /// <summary>
+        /// Encodes this instance.
+        /// </summary>
+        public List<byte> Encode()
+        {
+            this.Writer.WriteList(new Chest(Chest.Type.Free).Encode());
+            return this.Writer.Stream;
+        }
+
+        /// <summary>
+        /// Processes this instance.
+        /// </summary>
+        public void Process()
+        {
+            new Command(this.Type, this.Encode());
+        }
+    }
+}
