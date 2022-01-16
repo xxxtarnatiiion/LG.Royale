b80b0d98f9c40a2efa21fc63dd7ca687dd9ff539
xxxtarnatiiion <97824579+xxxtarnatiiion@users.noreply.github.com>
2022-01-16 16:55:48 +0900

    Create ClientHello.cs

---
+++ Core/Traffic/Packets/Client/Network/ClientHello.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using LG.Barbarian.Hype;
+
+namespace LG.Barbarian.Traffic
+{
+    class ClientHello
+    {
+
+        /// <summary>
+        /// Packet's ID.
+        /// </summary>
+        public static int Identifier
+        {
+            get
+            {
+                return 10101;
+            }
+        }
+
+        /// <summary>
+        /// Packet's new instance.
+        /// </summary>
+        public ClientHello()
+        {
+            this.Process();
+        }
+
+        /// <summary>
+        /// Processes this instance.
+        /// </summary>
+        public void Process()
+        {
+            new ServerHello();
+            new GetHomeData();
+        }
+    }
+}
