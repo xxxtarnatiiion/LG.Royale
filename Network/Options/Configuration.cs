b81a3518a5f4c749767570d415b953cb8ffc2d0e
xxxtarnatiiion <97824579+xxxtarnatiiion@users.noreply.github.com>
2022-01-16 17:34:30 +0900

    Create Configuration.cs

---
+++ Network/Options/Configuration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace LG.Royale.Network
+{
+    class Configuration
+    {
+
+        /// <summary>
+        /// Configuration's IP.
+        /// </summary>
+        public IPAddress IPAddress
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Configuration's port.
+        /// </summary>
+        public int Port
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Configuration's max allowed people number.
+        /// </summary>
+        public int MaxPeople
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Configuration's new instance.
+        /// </summary>
+        /// <param name="IPAddress"></param>
+        /// <param name="Port"></param>
+        /// <param name="MaxPeople"></param>
+        public Configuration(IPAddress IPAddress, int Port)
+        {
+            this.IPAddress = IPAddress;
+            this.Port      = Port;
+            this.MaxPeople = 150;
+        }
+    }
+}
