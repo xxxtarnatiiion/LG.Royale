f2c0aaa3244b68570549a737a297417dd1da267e
xxxtarnatiiion <97824579+xxxtarnatiiion@users.noreply.github.com>
2022-01-16 17:09:18 +0900

    Create RequestNewName.cs

---
+++ Hype/Traffic/Commands/Client/RequestNewName.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using LG.Royale.Hype;
+using LG.Royale.Interface;
+
+namespace LG.Royale.Packets
+{
+    class RequestNewName
+    {
+
+        /// <summary>
+        /// Packet's ID.
+        /// </summary>
+        public static int Identifier
+        {
+            get
+            {
+                return 14600;
+            }
+        }
+
+        /// <summary>
+        /// Packet's reader.
+        /// </summary>
+        public Reader Reader;
+
+        /// <summary>
+        /// Packet's new instance.
+        /// </summary>
+        public RequestNewName(byte[] Packet)
+        {
+            this.Reader = new Reader(Packet);
+
+            this.Decode();
+            this.Process();
+        }
+
+        /// <summary>
+        /// Packet's component.
+        /// </summary>
+        string NewName;
+
+        /// <summary>
+        /// Decodes this instance.
+        /// </summary>
+        public void Decode()
+        {
+            NewName = this.Reader.ReadString();
+        }
+
+        /// <summary>
+        /// Processes this instance.
+        /// </summary>
+        public void Process()
+        {
+            new NewNameOk(this.NewName);
+            Display.Log("Client's name changed to: " + this.NewName);
+        }
+    }
+}
