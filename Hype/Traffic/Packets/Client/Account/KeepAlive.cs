89ad8b171aa1694beadea5113b56ceed7fe4a884
xxxtarnatiiion <97824579+xxxtarnatiiion@users.noreply.github.com>
2022-01-16 17:12:31 +0900

    Create KeepAlive.cs

---
+++ Hype/Traffic/Packets/Client/Account/KeepAlive.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LG.Royale.Packets
+{
+    class KeepAlive
+    {
+
+        /// <summary>
+        /// Packet's ID.
+        /// </summary>
+        public static int Identifier
+        {
+            get
+            {
+                return 10108;
+            }
+        }
+
+        /// <summary>
+        /// Packet's new instance.
+        /// </summary>
+        public KeepAlive()
+        {
+            this.Process();
+        }
+
+        /// <summary>
+        /// Processes this instance.
+        /// </summary>
+        public void Process()
+        {
+            new KeepAliveOk().Send();
+        }
+    }
+}
