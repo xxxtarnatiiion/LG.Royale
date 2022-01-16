c52ef944b3726fde588f5db66563bf38829c0dae
xxxtarnatiiion <97824579+xxxtarnatiiion@users.noreply.github.com>
2022-01-16 16:50:38 +0900

    Create RequestNewName.cs

---
+++ Core/Traffic/Commands/Client/Account/RequestNewName.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using LG.Barbarian.Hype;
+
+namespace LG.Barbarian.Traffic
+{
+    class RequestNewName
+    {
+
+        /// <summary>
+        /// Packet's ID.
+        /// </summary>
+        public static int Identifier
+        {
+            get
+            {
+                return 14600;
+            }
+        }
+
+        /// <summary>
+        /// Packet's reader.
+        /// </summary>
+        public Reader Reader;
+
+        /// <summary>
+        /// Packet's new instance.
+        /// </summary>
+        public RequestNewName(byte[] Packet)
+        {
+            this.Reader = new Reader(Packet);
+            this.Decode();
+        }
+
+        /// <summary>
+        /// Packet's components.
+        /// </summary>
+        public static string NewName;
+
+        /// <summary>
+        /// Decodes this instance.
+        /// </summary>
+        public void Decode()
+        {
+            NewName = this.Reader.ReadString();
+            this.Process();
+        }
+
+        /// <summary>
+        /// Processes this instance.
+        /// </summary>
+        public void Process()
+        {
+            new SetNewName();
+        }
+    }
+}
