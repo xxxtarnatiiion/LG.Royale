a6ddfd50e61c407e7bf0a5bd5e08c7a8ee872b5d
xxxtarnatiiion <97824579+xxxtarnatiiion@users.noreply.github.com>
2022-01-16 17:13:02 +0900

    Create Login.cs

---
+++ Hype/Traffic/Packets/Client/Account/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using LG.Royale.Hype;
+
+namespace LG.Royale.Packets
+{
+    class Login
+    {
+
+        /// <summary>
+        /// Packet's ID.
+        /// </summary>
+        public static int Identifier
+        {
+            get
+            {
+                return 10101;
+            }
+        }
+
+        /// <summary>
+        /// Packet's new instance.
+        /// </summary>
+        public Login()
+        {
+            this.Process();
+        }
+
+        /// <summary>
+        /// Processes this instance.
+        /// </summary>
+        public void Process()
+        {
+            new LoginOk().Send();
+            new OwnHome().Send();
+        }
+    }
+}
