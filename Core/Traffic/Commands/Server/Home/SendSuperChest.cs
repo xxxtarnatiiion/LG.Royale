158f7e123a8fd4e40a14ea486a585e9bc0c64757
xxxtarnatiiion <97824579+xxxtarnatiiion@users.noreply.github.com>
2022-01-16 16:53:37 +0900

    Create SendSuperChest.cs

---
+++ Core/Traffic/Commands/Server/Home/SendSuperChest.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using LG.Barbarian.Hype;
+using LG.Barbarian.Logic;
+
+namespace LG.Barbarian.Traffic
+{
+    class SendSuperChest
+    {
+
+        /// <summary>
+        /// Packet's type.
+        /// </summary>
+        public int Type
+        {
+            get
+            {
+                return 210;
+            }
+        }
+
+        /// <summary>
+        /// Packet's writer.
+        /// </summary>
+        public Writer Writer;
+
+        /// <summary>
+        /// Packet's new instance.
+        /// </summary>
+        public SendSuperChest()
+        {
+            this.Writer = new Writer();
+            this.Process();
+        }
+
+        /// <summary>
+        /// Encodes this instance.
+        /// </summary>
+        public List<byte> Encode()
+        {
+            this.Writer.WriteList(new Chest(Chest.Type.Shop).Encode());
+            return this.Writer.Stream;
+        }
+
+        /// <summary>
+        /// Processes this instance.
+        /// </summary>
+        public void Process()
+        {
+            new Command(this.Type, this.Encode());
+        }
+    }
+}
