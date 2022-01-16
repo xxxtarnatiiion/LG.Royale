bdd940068c75c2cfeb2fbdb398d95264be02490c
xxxtarnatiiion <97824579+xxxtarnatiiion@users.noreply.github.com>
2022-01-16 16:59:09 +0900

    Create KeepAlive.cs

---
+++ Core/Traffic/Packets/Server/Network/KeepAlive.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using LG.Barbarian.Hype;
+using LG.Barbarian.Network;
+
+namespace LG.Barbarian.Traffic
+{
+    class KeepAlive
+    {
+
+        /// <summary>
+        /// Packet's ID.
+        /// </summary>
+        public int Identifier
+        {
+            get
+            {
+                return 20108;
+            }
+        }
+
+        /// <summary>
+        /// Packet's node.
+        /// </summary>
+        public int Node
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Packet's writer.
+        /// </summary>
+        public Writer Writer;
+
+        /// <summary>
+        /// Packet's new instance.
+        /// </summary>
+        public KeepAlive()
+        {
+            this.Writer = new Writer();
+            this.Process();
+        }
+
+        /// <summary>
+        /// Encodes this instance.
+        /// </summary>
+        public byte[] Encode()
+        {
+            return this.Writer.Write(this.Identifier, this.Node);
+        }
+
+        /// <summary>
+        /// Processes this instance.
+        /// </summary>
+        public void Process()
+        {
+            Protocol.Throw(this.Encode());
+        }
+    }
+}
